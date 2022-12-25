2d45ecc9c4b15ba61579ced3fb3cf072d69e3705
Bishal Shrestha <iambishal@yahoo.com>
2022-12-25 11:50:59 -0600

    wip

--- main.tf
+++ main.tf
@@ -1,26 +1,31 @@
 # Configure the Microsoft Azure Provider
-
-
-provider "azurerm" {
-  version = "=3.0.0"
-  features {}
-}
-
-
 terraform {
-  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+
+
   backend "azurerm" {
     resource_group_name  = "StorageRG"
     storage_account_name = "pathologyazuretf"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
-  } 
-    
+}
 }
 
 
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+}
+
 resource "azurerm_resource_group" "rg-hello-azure" {
   name     = "rg-hello-azure"
-  location = "northcentralus"
+  location = "eastus"
 }
 
