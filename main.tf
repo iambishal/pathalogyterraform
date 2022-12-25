c5bfd78cd5ee94ca01242275bf37cceb206a36cb
Bishal Shrestha <iambishal@yahoo.com>
2022-12-25 12:36:10 -0600

    wip

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 # Configure the Microsoft Azure Provider
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+    required_providers {
+      azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=3.0.0"
+      }
     }
+
+
+    backend "azurerm" {
+      resource_group_name  = "StorageRG"
+      storage_account_name = "pathologyazuretf"
+      container_name       = "terraform-state"
+      key                  = "terraform.tfstate"
   }
-
-
-  backend "azurerm" {
-    resource_group_name  = "StorageRG"
-    storage_account_name = "pathologyazuretf"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
-}
 }
 
 
@@ -24,8 +24,119 @@ provider "azurerm" {
 
 }
 
-resource "azurerm_resource_group" "rg-hello-azure" {
-  name     = "rg-hello-azure"
-  location = "eastus"
+
+resource "azurerm_resource_group" "pathology" {
+  name     = "pathology"
+  location = "East US"
+  tags = {
+    environment = "dev"
+  }
+}
+
+
+
+
+resource "azurerm_virtual_network" "pathlogy-vn" {
+  name                = "pathlogy-vn"
+  location            = azurerm_resource_group.pathology.location
+  resource_group_name = azurerm_resource_group.pathology.name
+  address_space       = ["10.0.0.0/16"]
+  #dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  tags = {
+    environment = "development"
+  }
+}
+
+resource "azurerm_subnet" "pathlogy-subnet1" {
+  name                 = "pathlogy-subnet1"
+  resource_group_name  = azurerm_resource_group.pathology.name
+  virtual_network_name = azurerm_virtual_network.pathlogy-vn.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+}
+
+
+resource "azurerm_network_security_group" "pathlogy-nsg1" {
+  name                = "pathlogy-nsg1"
+  location            = azurerm_resource_group.pathology.location
+  resource_group_name = azurerm_resource_group.pathology.name
+
+
+  tags = {
+    environment = "development"
+  }
+
+}
+
+resource "azurerm_network_security_rule" "pathlogySecurityRule1" {
+  name                        = "pathology-secrule1"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.pathology.name
+  network_security_group_name = azurerm_network_security_group.pathlogy-nsg1.name
+}
+
+
+
+resource "azurerm_subnet_network_security_group_association" "association-1" {
+  subnet_id                 = azurerm_subnet.pathlogy-subnet1.id
+  network_security_group_id = azurerm_network_security_group.pathlogy-nsg1.id
+}
+
+resource "azurerm_public_ip" "pathlogy-ip" {
+  name                = "pathlogy-ip"
+  resource_group_name = azurerm_resource_group.pathology.name
+  location            = azurerm_resource_group.pathology.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+
+resource "azurerm_network_interface" "pathlogy-ni1" {
+  name                = "pathlogy-ni1"
+  location            = azurerm_resource_group.pathology.location
+  resource_group_name = azurerm_resource_group.pathology.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.pathlogy-subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pathlogy-ip.id
+  }
+}
+
+
+
+resource "azurerm_linux_virtual_machine" "pathology-vm1" {
+  name                = "pathology-vm1"
+  resource_group_name = azurerm_resource_group.pathology.name
+  location            = azurerm_resource_group.pathology.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.pathlogy-ni1.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
 }
 
